bd15b45a2b67d0ada0efea943d77b0ca5c23450a
willcl-ark <will@256k1.dev>
2024-06-08 14:17:35 +0100

    scripts: update install_bitcoind.sh
    Install bins to /usr/local/bin directly from bitcoincore.org

--- scripts/install_bitcoind.sh
+++ scripts/install_bitcoind.sh
@@ -10,7 +10,27 @@ if [ -z "$BITCOIND_VERSION" ]; then
   exit 1
 fi
 
-docker pull lightninglabs/bitcoin-core:${BITCOIND_VERSION}
-CONTAINER_ID=$(docker create lightninglabs/bitcoin-core:${BITCOIND_VERSION})
-sudo docker cp $CONTAINER_ID:/opt/bitcoin-${BITCOIND_VERSION}.0/bin/bitcoind /usr/local/bin/bitcoind
-docker rm $CONTAINER_ID
+# Set the target platform
+TARGETPLATFORM=$(uname -m)
+if [ "$TARGETPLATFORM" = "x86_64" ]; then 
+    export TARGETPLATFORM="x86_64-linux-gnu"
+elif [ "$TARGETPLATFORM" = "arm64" ]; then 
+    export TARGETPLATFORM="arm64-apple-darwin"
+else
+    echo "Unsupported platform: $TARGETPLATFORM"
+    exit 1
+fi
+
+# Check if the version contains rc*
+if [[ $BITCOIND_VERSION == *rc* ]]; then
+    RC=$(echo "$BITCOIND_VERSION" | sed -E 's/.*(rc[0-9]+).*/\1/')
+    BITCOIND_VERSION_STRIPPED=${BITCOIND_VERSION%rc*}
+    URL="https://bitcoincore.org/bin/bitcoin-core-${BITCOIND_VERSION_STRIPPED}/test.${RC}/bitcoin-${BITCOIND_VERSION}-${TARGETPLATFORM}.tar.gz"
+else
+    RC=""
+    URL="https://bitcoincore.org/bin/bitcoin-core-${BITCOIND_VERSION}/bitcoin-${BITCOIND_VERSION}-${TARGETPLATFORM}.tar.gz"
+fi
+
+wget "$URL"
+tar zxvf "bitcoin-${BITCOIND_VERSION}-${TARGETPLATFORM}.tar.gz"
+sudo mv "bitcoin-${BITCOIND_VERSION}"/bin/* /usr/local/bin/
