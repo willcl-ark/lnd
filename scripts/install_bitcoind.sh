b1cb5b419a61a06cc5d4f53a9dd6f2c1230c050d
willcl-ark <will@256k1.dev>
2024-06-24 10:38:18 +0100

    ci: optimistically cache/restore bitcoind
    install_bitcoind.sh will restore from cache dir if binary exists for
    that bitcoind version and platform tuple.
    
    Only save cache on master branch, this makes them accessible to feature
    branches:
    https://github.com/actions/cache/blob/main/tips-and-workarounds.md#use-cache-across-feature-branches

--- scripts/install_bitcoind.sh
+++ scripts/install_bitcoind.sh
@@ -12,25 +12,41 @@ fi
 
 # Set the target platform
 TARGETPLATFORM=$(uname -m)
-if [ "$TARGETPLATFORM" = "x86_64" ]; then 
-    export TARGETPLATFORM="x86_64-linux-gnu"
-elif [ "$TARGETPLATFORM" = "arm64" ]; then 
-    export TARGETPLATFORM="arm64-apple-darwin"
+if [ "$TARGETPLATFORM" = "x86_64" ]; then
+  export TARGETPLATFORM="x86_64-linux-gnu"
+elif [ "$TARGETPLATFORM" = "arm64" ]; then
+  export TARGETPLATFORM="arm64-apple-darwin"
 else
-    echo "Unsupported platform: $TARGETPLATFORM"
-    exit 1
+  echo "Unsupported platform: $TARGETPLATFORM"
+  exit 1
 fi
 
-# Check if the version contains rc*
+# Define cache path.
+# This must exactly match that used in main.yml workflow.
+CACHE_DIR="bitcoind-${BITCOIND_VERSION}"
+
+# Check if bitcoind is already cached
+if [ -d "$CACHE_DIR" ]; then
+  echo "Using cached bitcoind version ${BITCOIND_VERSION} for ${TARGETPLATFORM}"
+  sudo cp "$CACHE_DIR"/bin/* /usr/local/bin/
+  exit 0
+fi
+
+# Download and extract bitcoind
 if [[ $BITCOIND_VERSION == *rc* ]]; then
-    RC=$(echo "$BITCOIND_VERSION" | sed -E 's/.*(rc[0-9]+).*/\1/')
-    BITCOIND_VERSION_STRIPPED=${BITCOIND_VERSION%rc*}
-    URL="https://bitcoincore.org/bin/bitcoin-core-${BITCOIND_VERSION_STRIPPED}/test.${RC}/bitcoin-${BITCOIND_VERSION}-${TARGETPLATFORM}.tar.gz"
+  RC=$(echo "$BITCOIND_VERSION" | sed -E 's/.*(rc[0-9]+).*/\1/')
+  BITCOIND_VERSION_STRIPPED=${BITCOIND_VERSION%rc*}
+  URL="https://bitcoincore.org/bin/bitcoin-core-${BITCOIND_VERSION_STRIPPED}/test.${RC}/bitcoin-${BITCOIND_VERSION}-${TARGETPLATFORM}.tar.gz"
 else
-    RC=""
-    URL="https://bitcoincore.org/bin/bitcoin-core-${BITCOIND_VERSION}/bitcoin-${BITCOIND_VERSION}-${TARGETPLATFORM}.tar.gz"
+  URL="https://bitcoincore.org/bin/bitcoin-core-${BITCOIND_VERSION}/bitcoin-${BITCOIND_VERSION}-${TARGETPLATFORM}.tar.gz"
 fi
 
 wget "$URL"
 tar zxvf "bitcoin-${BITCOIND_VERSION}-${TARGETPLATFORM}.tar.gz"
-sudo mv "bitcoin-${BITCOIND_VERSION}"/bin/* /usr/local/bin/
+
+# Move the binary to cache directory
+mkdir -p "$CACHE_DIR"
+cp -r "bitcoin-${BITCOIND_VERSION}"/* "$CACHE_DIR"/
+
+# Move the binary to the appropriate location
+sudo cp "$CACHE_DIR"/bin/* /usr/local/bin/
